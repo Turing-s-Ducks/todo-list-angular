51228d6b2674f9326a182164f746dedcdae5e9b9
Martin <alquimista3301@gmail.com>
2020-12-24 15:25:37 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo-list', component: TodoListComponent },
+  { path: '**', redirectTo:'todo-list'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
