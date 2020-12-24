dc55ae0d05838b45f6886a60434ff1ccc1b30ce5
Martin <alquimista3301@gmail.com>
2020-12-24 15:31:34 -0600

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [
-  { path: 'todo-list', component: TodoListComponent },
-  { path: '**', redirectTo:'todo-list'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
